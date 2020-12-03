b7772835b7e223515bbcdf12ec967e95f114a9c6
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-12-02 22:23:07 -0600

    timer mostly works correctly

--- src/views/TimerView.js
+++ src/views/TimerView.js
@@ -1,31 +1,160 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class TimerView extends Component {
+class TimerView extends React.Component {
   state = {
-    interval: {},
-    workTime: 0,
-    restTime: 0,
-    rounds: 0,
+    rounds: null,
+    restTime: null,
+    workTime: null,
+    currentTime: null,
+    currentRound: null,
+    isWork: true,
+    isRunning: false,
+  };
+
+  handleInputChange = (e) => {
+    switch (e.target.id) {
+      case 'rounds':
+        this.setState({
+          rounds: e.target.value,
+          currentRound: e.target.value,
+        });
+        break;
+      case 'workTime':
+        this.setState({ workTime: e.target.value });
+        this.setState({ currentTime: e.target.value });
+        break;
+      case 'restTime':
+        this.setState({ restTime: e.target.value });
+        break;
+      default:
+        this.manageInterval(e.target.id);
+        break;
+    }
+  };
+
+  manageInterval = (cmd) => {
+    if (
+      cmd === 'start'
+      && this.state.currentTime >= 0
+      && this.state.workTime > 0
+      && this.state.restTime > 0
+      && this.state.rounds > 0
+      && !this.state.isRunning
+    ) {
+      this.setState({ isRunning: true });
+      if (this.state.isWork) {
+        this.setState({ currentSetType: 'Work' });
+      }
+      this.timer = setInterval(() => {
+        if (this.state.currentTime === 0) {
+          if (this.state.isWork) {
+            this.setState({ currentSetType: 'Rest' });
+            this.setState({ currentTime: this.state.restTime });
+          } else {
+            this.setState({ currentSetType: 'Work' });
+            this.setState({ currentTime: this.state.workTime });
+            this.setState({ rounds: this.state.currentRound - 1 });
+            if (this.state.currentRound === 0) {
+              clearInterval(this.timer);
+              this.setState({
+                rounds: this.state.rounds,
+                restTime: this.state.restTime,
+                workTime: this.state.workTime,
+                currentRound: this.state.rounds,
+                currentSetType: null,
+                isRunning: false,
+              });
+            }
+          }
+          this.setState({ isWork: !this.state.isWork });
+          return;
+        }
+        this.setState({ currentTime: this.state.currentTime - 1 });
+      }, 1000);
+    } else if (cmd === 'stop') {
+      this.setState({
+        isRunning: false,
+      });
+    } else if (cmd === 'reset') {
+      this.setState({
+        rounds: null,
+        restTime: null,
+        workTime: null,
+        currentTime: null,
+        currentRound: null,
+        currentSetType: null,
+        isRunning: false,
+      });
+    }
   };
 
   render() {
     return (
-        <form>
-            <h2 className='d-flex justify-content-center'>Timer</h2>
-      <div className='d-flex flex-wrap justify-content-center my-3'>
-        <input type='text' name='workTime' placeholder='work time' />
-        <input type='text' name='restTime' placeholder='rest time'/>
-        <input type='text' name='rounds' placeholder='# of rounds'/>
+      <div>
+        <div className="timerDisplay" style={{ textAlign: 'center', marginTop: '15%' }}>
+          Timer Display
+          <div>Rounds: {this.state.rounds}</div>
+          <div>Work Time: {this.state.workTime}</div>
+          <div>Rest Time: {this.state.restTime}</div>
         </div>
-
-        <div className='d-flex flex-wrap justify-content-center'>
-        <button className='start mx-5'>Start</button>
-        <button className='stop mx-5'>stop</button>
-        <button className='reset mx-5'>reset</button>
-
-        <div class='clock'></div>
+        <div style={{ textAlign: 'center', marginTop: '15%' }}>
+          <div> Current Round: {this.state.currentRound}</div>
+          <div> {this.state.currentSetType}</div>
+          <div> Current Time: {this.state.currentTime}</div>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            marginTop: '25%',
+          }}
+        >
+          <div>
+            <label htmlFor='rounds'>Rounds:</label>
+            <input
+              onChange={this.handleInputChange}
+              type='number'
+              id='rounds'
+              name='rounds'
+              min='1'
+              max='15'
+            />
+          </div>
+          <div>
+            <label htmlFor='workTime'>Work Time (sec):</label>
+            <input
+              onChange={this.handleInputChange}
+              type='number'
+              id='workTime'
+              name='workTime'
+              min='1'
+              max='360'
+            />
+          </div>
+          <div>
+            <label htmlFor='restTime'>Rest Time (sec):</label>
+            <input
+              onChange={this.handleInputChange}
+              type='number'
+              id='restTime'
+              name='restTime'
+              min='1'
+              max='360'
+            />
+          </div>
+          <button id='start' onClick={this.handleInputChange}>
+            Start
+          </button>
+          <button id='stop' onClick={this.handleInputChange}>
+            Stop
+          </button>
+          <button id='reset' onClick={this.handleInputChange}>
+            Reset
+          </button>
+        </div>
       </div>
-      </form>
     );
   }
 }
+
+export default TimerView;
